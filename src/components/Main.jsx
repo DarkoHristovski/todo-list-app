4ffa8f42f7bb86eff2d2ba3e8125fd4ba1e69fb1
DarkoHristovski <hristovski15@gmail.com>
2023-11-20 10:45:26 +0100

    new css imporvments to the site

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,11 +6,13 @@ import { Routes, Route } from 'react-router-dom'
 const Main = ({todo, deleteTask,setEditPopUp,editTask, setTodo,importFilter}) =>{
   return ( 
   <main className="main">
+    <div className='main-wrapper'>
     <Routes>
     <Route path="/" element={todo.map(x=> <TodoItem editTask={editTask} setEditPopUp={setEditPopUp} deleteTask={deleteTask} setTodo={setTodo} key={x.id} content={x}/>)}/>
     <Route path="/important" element={importFilter.map(x=> <ImportantToDo editTask={editTask} setEditPopUp={setEditPopUp} deleteTask={deleteTask} setTodo={setTodo} key={x.id} content={x}/>)}/>
     <Route path="/completed" element={todo.map(x=> <TodoItem editTask={editTask} setEditPopUp={setEditPopUp} deleteTask={deleteTask} setTodo={setTodo} key={x.id} content={x}/>)}/>
    </Routes> 
+   </div>
    </main>  
    )
 }
